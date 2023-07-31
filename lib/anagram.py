713fe4541b0ebdd5bdf717044657b0c2f1237065
John DH. Park <john.dh.park@icloud.com>
2023-07-31 05:33:42 -0700

    Completed Anagram Detector Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,27 @@
 # your code goes here!
+
+class Anagram:
+	def __init__(self, word):
+		self.word = word
+		self.char_count = self.count_chars(self.word)
+
+	def match(self, words):
+		matching_words = []
+
+		for word in words:
+			curr_count = self.count_chars(word)
+
+			if self.char_count == curr_count:
+				matching_words.append(word)
+
+		return matching_words
+
+	def count_chars(self, word):
+		char_count = [0 for _ in range(25)]
+
+		for char in word:
+			idx = ord(char) - 97
+			char_count[idx] += 1
+
+		return char_count
+
